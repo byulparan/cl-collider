4fed466e766dbeca3e8c6f2816990af26efce161
byulparan <byul@byulparans-MacBook-Pro.local>
2017-03-19 19:34:16 +0900

    bug fix UGen 'PMOsc.ar'

--- ugens/pmosc.lisp
+++ ugens/pmosc.lisp
@@ -3,7 +3,7 @@
 
 
 (defun pm-osc.ar (car-freq mod-freq &optional (pm-index 0.0) (mod-phase 0.0) (mul 1.0) (add 0.0))
-  (sin-osc.ar car-freq (sin-osc mod-freq mod-phase pm-index) mul add))
+  (sin-osc.ar car-freq (sin-osc.ar mod-freq mod-phase pm-index) mul add))
 
 (defun pm-osc.kr (car-freq mod-freq &optional (pm-index 0.0) (mod-phase 0.0) (mul 1.0) (add 0.0))
   (sin-osc.kr car-freq (sin-osc.kr mod-freq mod-phase pm-index) mul add))
