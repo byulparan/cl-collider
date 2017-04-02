a432fe203811d2ff93059bf89daa0d81933d6873
byulparan <byul@byulparans-MacBook-Pro.local>
2017-04-02 19:51:40 +0900

    fix typo in 'ugens/audioin.lisp'

--- ugens/audioin.lisp
+++ ugens/audioin.lisp
@@ -2,10 +2,10 @@
 (in-package #:sc)
 
 (defun sound-in-prim (chan-offset bus mul add)
-  (if (not (listp bus)) (sc::madd (in (sc::add chan-offset bus) 1) mul add)
-	(if (equal bus (loop repeat (length bus) for i from (car bus) collect i))
-	    (sc::madd (in (sc::add chan-offset (car bus)) (length bus)) mul add)
-	    (sc::madd (in (sc::add chan-offset bus)) mul add))))
+  (if (not (listp bus)) (sc::madd (in.ar (sc::add chan-offset bus) 1) mul add)
+    (if (equal bus (loop repeat (length bus) for i from (car bus) collect i))
+	(sc::madd (in.ar (sc::add chan-offset (car bus)) (length bus)) mul add)
+      (sc::madd (in.ar (sc::add chan-offset bus)) mul add))))
 
 (defun sound-in.ar (&optional (bus 0) (mul 1.0) (add 0.0))
   (let ((chan-offset (num-output-buses.ir)))
