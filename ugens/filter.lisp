16f4d6140106c909db55c3c4ff84d501dccfa87a
modula t. worm <defaultxr@gmail.com>
2018-01-18 18:49:24 -0600

    add RLPFD UGen

--- ugens/filter.lisp
+++ ugens/filter.lisp
@@ -346,3 +346,11 @@
    (:kr
     (madd (multinew new 'pure-ugen in freq gain reset) mul add)))
   :check-fn #'check-same-rate-as-first-input)
+
+(defugen (rlpfd "RLPFD")
+    (in &optional (ffreq 440) (res 0) (dist 0) (mul 1) (add 0))
+  ((:ar
+    (madd (multinew new 'pure-ugen in ffreq res dist) mul add))
+   (:kr
+    (madd (multinew new 'pure-ugen in ffreq res dist) mul add)))
+  :check-fn #'check-same-rate-as-first-input)
