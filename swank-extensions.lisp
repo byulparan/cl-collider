5ef57c74015f1e7ed1a0e817dfc06ba39853a48a
Lou Woell <lou.woell@posteo.de>
2023-11-07 16:08:29 +0100

    Make slime and sly show all arguments for `synth`

--- swank-extensions.lisp
+++ swank-extensions.lisp
@@ -1,7 +1,8 @@
 (in-package #:cl-collider)
 
 ;; make slime show the synthdef's argument list for (synth ...)
-(defmethod swank::compute-enriched-decoded-arglist ((operator-form (eql 'synth)) argument-forms)
+(defmethod swank::compute-enriched-decoded-arglist ((operator-form (eql 'synth))
+                                                    argument-forms)
   (let* ((fst (car argument-forms))
          (controls (unless (typep fst 'swank::arglist-dummy)
                      (synthdef-metadata (if (and (listp fst)
@@ -10,11 +11,22 @@
                                             fst)
                                         :controls))))
     (if controls
-        (let ((req (loop :for ctl :in controls
-                      :if (atom ctl)
-                      :collect ctl))
-              (key (loop :for ctl :in controls
-                      :if (listp ctl)
-                      :collect (swank::make-keyword-arg (alexandria:make-keyword (car ctl)) (car ctl) (cadr ctl)))))
-          (swank::make-arglist :required-args (append (list fst) req) :key-p t :keyword-args key))
+        (loop
+          :for ctl :in controls
+          :if (atom ctl)
+            :collect ctl :into req
+          :if (listp ctl)
+            :collect (swank::make-keyword-arg
+                      (alexandria:make-keyword (car ctl))
+                      (car ctl)
+                      (cadr ctl))
+              :into key
+          :finally
+             (return
+               (swank::make-arglist
+                :required-args (append (list fst) req)
+                :key-p t
+                :keyword-args (append
+                               key
+                               (swank::keywords-of-operator operator-form)))))
         (call-next-method))))
