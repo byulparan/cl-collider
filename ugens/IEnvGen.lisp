6559404eee2b9bb3b803a5788ead618b4eb57a39
byulparan <byulparan@gmail.com>
2024-06-03 18:15:11 +0900

    fixed i-env-gen.kr to use correct curve-number

--- ugens/IEnvGen.lisp
+++ ugens/IEnvGen.lisp
@@ -2,7 +2,7 @@
 
 (defun as-array-for-interpolation (env)
   "Convert ENV into an array in a format suitable for i-env-gen."
-  (with-slots (levels times curve-value) env
+  (with-slots (levels times curve-number curve-value) env
     (let* ((size (length times))
            (contents (append (list
                               0 ;; Env offset (not implemented in cl-collider)
@@ -11,7 +11,7 @@
                               (reduce #'+ times))
                              (loop :for i :from 0 :below size
                                 :append (list (elt times i)
-                                              (env-shape-number (nth-wrap i curve-value))
+					      (nth-wrap i curve-number)
                                               (nth-wrap i curve-value)
                                               (elt levels (1+ i)))))))
       (mapcar (lambda (list)
@@ -21,6 +21,6 @@
 (defugen (i-env-gen "IEnvGen")
     (envelope index &optional (mul 1.0) (add 0.0))
   ((:ar (madd (unbubble (mapcar #'process-env (multinew new 'ugen index
-						                                (as-array-for-interpolation envelope)))) mul add))
+						        (as-array-for-interpolation envelope)))) mul add))
    (:kr (madd (unbubble (mapcar #'process-env (multinew new 'ugen index
-						                                (as-array-for-interpolation envelope)))) mul add))))
+						        (as-array-for-interpolation envelope)))) mul add))))
