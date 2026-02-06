81a525204b9d76b9b0df5e3c44ff5de861324127
Sungmin Park <byulparan@gmail.com>
2026-02-06 17:16:11 +0900

    Update dyn-gen API

--- ugens/Extensions/DynGen.lisp
+++ ugens/Extensions/DynGen.lisp
@@ -2,73 +2,122 @@
 
 (in-package :sc)
 
-(export '(list-all-dyn-gen def-dyn-gen dyn-gen.ar dyn-gen-rt.ar))
+(export '(dyn-gen-all dyn-gen-send dyn-gen-load dyn-gen-free dyn-gen-free-all dyn-gen.ar))
 
 
-(defvar *dyn-gen-table* (make-hash-table))
-
-(defun list-all-dyn-gen ()
-  "Return all dyn-gen objects"
-  (alexandria:hash-table-values *dyn-gen-table*))
+(defvar *dyn-gen-register-table* (make-hash-table))
 
 
-(defstruct (dyn-gen
-	    (:constructor %make-dyn-gen (name hash code)))
-  name hash code)
-
-
-(defun name-hash (name)
+(defun dyn-gen-hash (name)
   (flet ((hash (name)
-	   (mod (sxhash name) (expt 2 20))))
+	   (let* ((h (sxhash name) ))
+	     (* (mod h (expt 2 20)) (sign h)))))
     (let* ((h (hash name)))
-      (loop for n in (remove name (alexandria:hash-table-keys *dyn-gen-table*))
+      (loop for n in (remove name (alexandria:hash-table-keys *dyn-gen-register-table*))
 	    when (= h (hash n))
 	      do (error "Conflict hash key ~a and ~a." name n))
       h)))
 
 
-(defun def-dyn-gen (name code)
+(defun dyn-gen-all ()
+  "Return all dyn-gen names"
+  (loop for k being the hash-keys of *dyn-gen-register-table*
+	  using (hash-value v)
+	collect (list :name k :params v)))
+
+
+
+;; extract param
+(defun dyn-gen-code-optimized (code)
+  (setf code (ppcre:regex-replace-all "\\/\\/.*?$" code ""))
+  (setf code (ppcre:regex-replace-all "\\/\\*.*?\\*\\/" code ""))
+  (let* ((result nil))
+    (ppcre:do-scans (match-start match-end reg-starts reg-ends "[^(?:A-Za-z|\\_|$|]?(\\_(?:[A-Za-z]|[0-9]|_)+)" code)
+      (pushnew (subseq code (aref reg-starts 0) (aref reg-ends 0)) result :test #'string=))
+    (nreverse result)))
+
+
+
+(defun dyn-gen-send (name code)
   "In order to run a DynGen script on the server it is necessary to first register it on the server under a given name, similar to a SynthDef.
 If the code for an already existing name gets updated, all running instances of this code will also be updated. This allows to live-code DynGen scripts."
   (assert (and (boot-p *s*) (is-local-p *s*)))
-  (let* ((hash (name-hash name))
-	 (dyn-gen (%make-dyn-gen name hash code)))
-    (setf (gethash name *dyn-gen-table*) dyn-gen)
-    (uiop:with-temporary-file (:stream stream
-			       :pathname p)
-      (format stream "~a" code)
-      (close stream)
-      (send-message *s* "/cmd" "dyngenadd" (floatfy hash) (namestring p))
-      (sync))
-    dyn-gen))
+  (let* ((hash (dyn-gen-hash name))
+	 (params (dyn-gen-code-optimized code))
+	 (message (append (list "/cmd" "dyngenscript" hash code (length params)) params (list nil)))
+	 (script-msg (apply #'osc:encode-message message)))
+    (if (< (length script-msg) (/ 65535 4)) (let* ((osc-device (osc-device *s*)))
+					      (usocket:socket-send (sc-osc::socket osc-device) script-msg (length script-msg)
+								   :port (sc-osc::port osc-device)
+								   :host (sc-osc::host osc-device)))
+      (progn
+	(unless (is-local-p *s*)
+	  (error "DynGenDef ~a could not be added  to server because it is too big for sending via OSC and server is not local" name))
+	(uiop:with-temporary-file (:stream stream
+				   :pathname p)
+	  (format stream code)
+	  (close stream)
+	  (let* ((message (append (list "/cmd" "dyngenfile" hash (namestring p) (length params)) params (list nil))))
+	    (apply #'send-message *s* message)))))
+    (sync)
+    (setf (gethash name *dyn-gen-register-table*) params)
+    name))
 
 
 
-(defun meta-dyn-gen (ugen-name num-outputs script realtime inputs)
-  (let* ((hash (cond ((dyn-gen-p script) (dyn-gen-hash script))
-		     (t (name-hash script)))))
-    (apply #'ugen-new ugen-name :audio 'multiout-ugen #'identity :bipolar
-	   num-outputs hash realtime
-	   (mapcar (lambda (in) (if (eql :audio (rate in)) in
-				  (k2a.ar in)))
-		   inputs))))
+(defun dyn-gen-load (name pathname)
+  (dyn-gen-send name (alexandria:read-file-into-string pathname)))
 
 
-(defun dyn-gen.ar (num-outputs script &rest inputs)
+
+(defun dyn-gen-free (name)
+  "Unregisters a DynGen script from the server."
+  (send-message *s* "/cmd" "dyngenfree" (dyn-gen-hash name))
+  (remhash name *dyn-gen-register-table*)
+  (sync)
+  name)
+
+
+(defun dyn-gen-free-all ()
+  "Unregisters all DynGen scripts from the server."
+  (send-message *s* "/cmd" "dyngenfreeall")
+  (setf *dyn-gen-register-table* (make-hash-table))
+  (sync))
+
+
+
+(defun dyn-gen.ar (num-output name &key inputs params (update 1.0) (sync 0.0))
   "This UGen evaluates EEL2 (expression evaluation library/realtime compiler) code in a VM which runs on the server. This allows to write DSP code on the fly and perform single sample operations.
 
 The code has to be registered using DynGenDef and follows a similar approach to SynthDef by registering a resource (code) under a given name. This also allows to update the evaluated code on the fly.
 
 Each script can expose multiple outputs and the output for each channel can be written to via the variable out0, out1, ... When the script gets evaluated through DynGen it is also necessary to tell how many outputs of the script should be exposed."
-  (meta-dyn-gen "DynGen" num-outputs script 0.0 inputs))
+  (multiple-value-bind (spec registed-p)
+      (gethash name *dyn-gen-register-table*)
+    (assert registed-p nil "Can't found dyn-gen module ~a" name)
+    (let* ((param-spec spec)
+	   (inputs (alexandria:ensure-list inputs))
+	   (params (alexandria:ensure-list params)))
+      (when params
+	(assert (evenp (length params)) nil "params should be pair of key, value"))
+      (setf params
+	(loop for (key value) on params by #'cddr
+	      for index = (or (position (format nil "_~a" key) param-spec :test #'string=)
+			      (position key param-spec :test #'string=))
+	      do (assert index nil "Can't found param ~s in dyn-gen module ~a" key name)
+	      append (list index (if (not (eql :audio (rate value))) (k2a.ar value) value))))
+      (setf inputs (mapcar (lambda (sig) (if (not (eql :audio (rate sig))) (k2a.ar sig) sig)) inputs))
+      (apply 'ugen-new "DynGen" :audio 'multiout-ugen #'identity :bipolar
+	     num-output
+	     (dyn-gen-hash name)
+	     update
+	     sync
+	     (length inputs)
+	     (/ (length params) 2)
+	     (append inputs params)))))
 
 
 
-(defun dyn-gen-rt.ar (num-outputs script &rest inputs)
-  "See DynGen for a full introduction.
-
-DynGen defers the initialization of the EEL2 VM into a non-realtime thread. This introduces a delay of at least one block size. If therefore sample accurate sequencing becomes necessary, it is also possible to init the EEL2 VM within the realtime-audio thread, though this can lead to audio-dropouts and is therefore not advised. "
-  (meta-dyn-gen "DynGenRT" num-outputs script 1.0 inputs))
 
 
 
